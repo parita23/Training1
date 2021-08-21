f253fc7cb835ad469b0f4a3ae8b2b1020fea755f
parita23 <88388510+parita23@users.noreply.github.com>
2021-08-21 18:29:26 +0530

    plugin task
    today i have done some plugin task like validation form and datatble etc.

---
+++ 21-08-2021/Magnifig Popup/js/active.js
@@ -0,0 +1,17 @@
+(function ($) {
+"use strict";
+
+/*--------------------------------------
+	Magnific popup Active
+----------------------------------------*/
+$('.img-thumbnail').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery:{
+		enabled:true
+	}
+});
+
+})(jQuery);	
+
+
