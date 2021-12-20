685293daeaf0d1ed5b7f9a406c415532b0bf5d4c
WebCodeGenie <parita.g@webcodegenie.com>
2021-12-20 18:58:05 +0530

    import done

--- december/13-12-2021/loginAuthenticate/loginAuthenticate/public/javascripts/api.js
+++ december/13-12-2021/loginAuthenticate/loginAuthenticate/public/javascripts/api.js
@@ -4,8 +4,6 @@ $(Document).ready(() => {
         url: "/getAllUsers",
         type: "GET",
         success: function (data) {
-            console.log("hiiiii")
-            console.log("data", data);
             //for get the single data from array
             for (const user of data.result) {
                 let temp1 = `<tr>
@@ -21,7 +19,6 @@ $(Document).ready(() => {
     });
     //for export the csv file
     $(".export").on("click", function () {
-        console.log("click")
         var myObj = {
             type: "exporting",
         };
@@ -32,9 +29,7 @@ $(Document).ready(() => {
             success: function (data) {
                 var link = $("<a />");
                 link.attr("download", "parita.csv");
-                console.log("data.fileName", data.fileName)
                 let url = "http://127.0.0.1:3000/csvFile/" + data.fileName;
-                console.log("url", url)
                 link.attr("href", url);
                 $("body").append(link);
                 link[0].click();
@@ -126,7 +121,6 @@ $(Document).ready(() => {
         },
         //when we want to place our error message
         errorPlacement: function (error, element) {
-            console.log('dd', element.attr("name"))
 
             if ($(element).attr('name') == 'name') {
                 error.insertAfter(element.parent());
@@ -180,16 +174,14 @@ $(Document).ready(() => {
             file: {
                 required: true,
                 extension: "csv"
-            }
+            },
         },
         messages:
         {
             file: {
                 required: "Please select the file",
-                extension: "please select only csv file"
-
-            }
-
+                extension:"please select csv file"
+            },
         },
         highlight: function (element) {
             $(element).addClass("errorClass");
@@ -198,13 +190,13 @@ $(Document).ready(() => {
             $(element).removeClass("errorClass");
         },
         errorPlacement: function (error, element) {
-            console.log("dd", element.attr("name"));
-
-            error.insertAfter(element.parent());
+           
+                error.insertAfter(element.parent());
+            
         },
         submitHandler: function (form, event) {
             event.preventDefault();
-
+            //for taking file data
             var demoData = new FormData();
             demoData.append("file", $("#file")[0].files[0]);
 
@@ -215,45 +207,30 @@ $(Document).ready(() => {
                 contentType: false,
                 processData: false
             }).done(function (data) {
-                console.log("hiiiii");
-                console.log("datadata",data)
+                //set Data lable id as fileId
                 $(".dataId").attr("fileId", data.id);
-
+                //for appending the dropdown and checkbox 
                 for (let index = 0; index < data.firstRow.length; index++) {
-                    
                     $('.csvTbl').append("<tr class='table-danger'> <td><input type='checkbox' value=" + data.firstRow[index] + " class='form-check-input' ></td> <td>" + data.firstRow[index] + "</td> <td>" + data.secondRow[index] +
                         "</td> <td>  <select class='form-select'id=" + data.firstRow[index] + "-dropdown > <option value='' disabled >Select Value</option> <option value='name'>name</option> <option value='email'>email</option> <option value='mobile'>mobile</option> </select></td></tr>");
                 };
             });
-
-            //execute when click on export for csv download
         },
 
     })
-
+    //upload click event
     $(".upload").click(function () {
-        console.log("upload button click")
+        //we set our fileId to as form class dataId
         let fileId = $(".dataId").attr("fileId");
        
-        var keyArray = [];
-        $("input:checkbox:checked").each(function () {
-            keyArray.push($(this).val());
-          });
-          
-          console.log("keyArray...",keyArray);
-         
+        //we get the value of checkbox and dropDown value and put as fieldMap[checkboxval] = dropdownVal
           let fieldMap = {}
             $("input:checked").each(function () {
                 let checkboxval = $(this).val()
-                console.log('check -------- ', checkboxval);
                 let dropdownVal = $(`#${checkboxval}-dropdown option:selected`).val();
-                console.log('dropdown value -------- ',dropdownVal);
                 fieldMap[checkboxval] = dropdownVal;
-            
             });
-            console.log('fieldMap-------', fieldMap,fileId)
-
-
+            //for mapping ajax call
             $.ajax({
                 url: "/mapping/"+fileId,
                 type: "POST",
--- december/13-12-2021/loginAuthenticate/loginAuthenticate/routes/index.js
+++ december/13-12-2021/loginAuthenticate/loginAuthenticate/routes/index.js
@@ -1,15 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const userModel = require("../models/userModel");
 const fileModel = require("../models/fileModel");
-const jwt = require('jsonwebtoken');
-const { check } = require('express-validator');
+const jwt = require("jsonwebtoken");
+const { check } = require("express-validator");
 const { Parser } = require("json2csv");
 var moment = require("moment");
 const fs = require("fs");
 const multer = require("multer");
 
-const csv = require('csvtojson')
+const csv = require("csvtojson");
 
 //for uploading the file for importing(using multer)
 const storage = multer.diskStorage({
@@ -28,33 +28,34 @@ function authJWT(req, res, next) {
   const privateKey = "ujtqcfrdeujbjgdbhghjdbfghdjfuygfbjgfhbfghg";
   jwt.verify(token, privateKey, function (err, user) {
     if (err) {
-      res.redirect("/loginApi")
+      res.redirect("/loginApi");
     } else {
-      req.user=user
+      req.user = user;
       next();
     }
-  })
+  });
 }
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/signup', authJWT, async function (req, res, next) {
-  res.render('signup');
+router.get("/signup", authJWT, async function (req, res, next) {
+  res.render("signup");
 });
 
-router.get('/loginApi', function (req, res, next) {
+router.get("/loginApi", function (req, res, next) {
   //if user once login and go to the signup then they can not go back for  login
   let token = req.cookies.token;
+  console.log("token...",token);
   if (token) {
-    res.redirect("/signup")
+    res.redirect("/signup");
   }
-  res.render('login');
+  res.render("login");
 });
 //getAllUsers api is created for list all data from database which are already exit in database
-router.get('/getAllUsers', authJWT, async function (req, res, next) {
+router.get("/getAllUsers", authJWT, async function (req, res, next) {
   //find all the data
   let allData = await userModel.find().lean();
   //if for exporting the json to csv file
@@ -70,11 +71,9 @@ router.get('/getAllUsers', authJWT, async function (req, res, next) {
     const csv = parser.parse(allData);
     //in fileName store the data in which format you want to dowanload.
     //moment() is used for current time means appent at a time csv.for that we install npm momement
-    let fileName =
-      "exportData-" + moment().format("YYYY-MM-DD hh:mm") + ".csv";
-    console.log("bad luck", fileName)
+    let fileName = "exportData-" + moment().format("YYYY-MM-DD hh:mm") + ".csv";
     //we create one folder csvFile for uploading our csv to that folder
-    fs.writeFileSync("public/csvFile/" + fileName, csv)
+    fs.writeFileSync("public/csvFile/" + fileName, csv);
     res.send({
       type: "success",
       result: allData,
@@ -89,237 +88,261 @@ router.get('/getAllUsers', authJWT, async function (req, res, next) {
 //importing the csv to json data and for that we first upload our file to the public folder file
 router.post("/import", authJWT, upload.single("file"), async (req, res) => {
   try {
+    //in if we get the user.userId from loginApi and generate in authjWt
     if (req.file) {
       const temp = {
         name: req.file.filename,
-        uploadedBy:req.user.userId,
-  
-  
+        uploadedBy: req.user.userId,
       };
       var data = await fileModel.create(temp);
-      console.log('data....',data);
-      console.log("req.file", req.file)
-      
+     
       //csvFilePath is get the path of file which is upload in one file
-      const csvFilePath = req.file.destination + "/" + req.file.filename
-      console.log("csvFilePath", csvFilePath)
-  
+      const csvFilePath = req.file.destination + "/" + req.file.filename;
+      
       //in jsonArray we get our json file
       const jsonArray = await csv().fromFile(csvFilePath);
-      console.log("jsonArrayyyyyyyyyyyyyy", jsonArray);
-  
-      if(jsonArray){
-        
-        let firstRow=Object.keys(jsonArray[0])
-        let secondRow=Object.values(jsonArray[0])
-  
-      res.send({
-        type: "success",
-        firstRow:firstRow,
-        secondRow:secondRow,
-        id:data._id
-      })
-      }
-    
-    }
-  } catch (error) {
-    console.log("error",error);
-  }
- 
-})
 
-//mapping route
+      if (jsonArray) {
+        let firstRow = Object.keys(jsonArray[0]);
+        let secondRow = Object.values(jsonArray[0]);
 
-router.post("/mapping/:data", authJWT, async (req, res) => {
- console.log("mapping route..........")
- console.log(req.params.data);
- console.log(req.body)
- 
- let data1=await fileModel.findOne({_id:req.params.data})
- console.log("data1",data1);
-
- const csvFilePath = "./public/importFile/" + data1.name
- console.log("csvFilePath", csvFilePath)
-
- //in jsonArray we get our json file
- const jsonArray = await csv().fromFile(csvFilePath);
-//  console.log("jsonArrayyyyyyyyyyyyyy", jsonArray);
-
- if(jsonArray){
-  let totalRecords=0
-  let duplicates=0
-  let discarded=0
-  let totalUploaded=0
-   let mappedArray={}
-  
-
-  let jsonKeys=Object.keys(req.body)
-  let jsonValues=Object.values(req.body)
-
-  for(let i=0;i<jsonArray.length;i++){
-   
-    totalRecords++
-
-    // console.log(" after totalll",totalRecords)
-    // console.log("iiiiiiiiiii",jsonArray[i])
-    for (let key = 0; key < jsonValues.length; key++) {
-      let jsonObj = jsonArray[i];
-
-
-       let mapObj = jsonValues[key];
-      
-       let keyOfMapObject = jsonKeys[key];
-       
-       req.body[keyOfMapObject] = jsonObj[mapObj];
-
-      //  console.log("jjjjjjj",keyOfMapObject)
-    }
-    // console.log("req.body.email..",req.body.email)
-    // console.log("req.body.mobile..",req.body.mobile)
-
-
-    let user = await userModel.findOne({
-      //user can login mobile and email both
-      $or: [{ email: req.body.email }, { mobile: req.body.mobile }]
-    })
-    console.log("duplicates..",user)
-    // console.log("req.bodyyy",req.body)
-
-    var emailRegExpression = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-
-    if (user) {
-      duplicates++;
-    } else if (
-      emailRegExpression.test(req.body.name) == true ||
-      req.body.name == null ||
-      req.body.name == ""
-    ) {
-      console.log("Name is Invalid");
-      discarded++;
-    } else if (emailRegExpression.test(req.body.email) == false) {
-      console.log("Email is Invalid");
-      discarded++;
-    } else if (
-      req.body.mobile.length < 10 ||
-      req.body.mobile.length > 10
-    ) {
-      console.log("Mobile must be 10 digits only.");
-      discarded++;
-    } else {
-      mappedArray.push(req.body);
-      totalUploaded++;
-    }
-  }
-
-
-      
- 
- }
-
- 
-})
-
-//signup
-router.post("/signup", [
-  check("email", "Email length should be 10 to 30 characters")
-    .isEmail()
-    .isLength({ min: 10, max: 30 }),
-  check("name", "Name length should be 10 to 20 characters").isLength({
-    min: 2,
-    max: 20,
-  }),
-  check("mobile", "Mobile number should contains 10 digits").isLength({
-    min: 10,
-    max: 10,
-  }),
-  check("password", "Password length should be 8 to 10 characters").isLength({
-    min: 6,
-    max: 10,
-  }),
-], authJWT, async (req, res) => {
-
-  try {
-
-      //code for add the data
-    console.log("req.body", req.body)
-  const item = {
-    name: req.body.name,
-    email: req.body.email,
-    mobile: req.body.mobile,
-    password: req.body.password,
-  };
-
-  let data = await userModel.findOne({
-    //check if email or mobile is already exist or not
-    $or: [{ email: req.body.email }, { mobile: req.body.mobile }]
-  })
-  if (data) {
-    res.send({
-      type: "error",
-      message: "this email or mobile number already used...",
-    });
-  } else {
-    let dataStore = userModel(item);
-    dataStore.save(async function (err, result) {
-      if (err) {
-        console.log(err);
-      } else {
         res.send({
           type: "success",
-          result: dataStore,
+          firstRow: firstRow,
+          secondRow: secondRow,
+          id: data._id,
         });
       }
+    } else {
+      res.json({
+        status: "error",
+        code: 404,
+        message: "file not found",
+      });
+    }
+  } catch (error) {
+    res.json({
+      status: "error",
+      code: 404,
+      message: "data not found",
     });
   }
+});
 
-  console.log(item);
-  } catch (error) {
-    console.log("error..",error)
-  }
-
+//mapping route for compare database and csv field
+///mapping/:data in data we get id from ajax and access as req.params
+router.post("/mapping/:data", authJWT, async (req, res) => {
+  try {
+    let validUser = 0;
+    let invalidUser = 0;
+    let duplicateUser = 0;
+    let discarded = 0;
+    let totalRecords = 0;
+    let finalUser = [];
+    //we find which file is uploaded (Id)
+    let result = await fileModel.findOne({ _id: req.params.data });
+   //find the csv path from public folder
+    const csvFilePath = "./public/importFile/" + result.name;
   
-})
-//for login api and route
-router.post("/loginApi",
+    //in jsonArray we get our json file
+    const jsonArray = await csv().fromFile(csvFilePath);
+    
+    if (jsonArray) {
+      for (let user of jsonArray) {
+        //we find totalRecords in csv
+        totalRecords++;
+        //we take one email regExpression for checking valid field data or not
+        var emailRegExpression = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        //we check name,email and mobile exist in csv or not
+        if (user.name && user.email && user.mobile) {
+
+          //if we get that data from database then go to  as duplicate
+          let data = await userModel.findOne({
+            $or: [{ email: user.email }, { mobile: user.mobile }],
+          });
+         
+          if (data) {
+            duplicateUser++;
+  
+          } else if (
+            //we check in name there is any unwanted character is there if yes then name is invalid and go to discarded
+            emailRegExpression.test(req.body.name) == true ||
+            req.body.name == null ||
+            req.body.name == ""
+          ) {
+            
+            discarded++;
+          } else if (emailRegExpression.test(req.body.email) == false) {
+            //check email regExpression is valid
+            
+            discarded++;
+          } else if (
+            //check mobile number length is exactly 10 or not
+            req.body.mobile.length < 10 ||
+            req.body.mobile.length > 10
+          ) {
+            discarded++;
+          } else {
+      //if our data is proper then go to as valid user
+            validUser++;
+            let userObj = {};
+            //creating mapping fields for check our database field match to csv fields
+            userObj[req.body.name] = user.name;
+            userObj[req.body.email] = user.email;
+            userObj[req.body.mobile] = user.mobile;
+            finalUser.push(userObj);
+          }
+        } else {
+          invalidUser++;
+        }
+      }
+      //find total uploaded user which is clean
+      let uploadedUsers = await userModel.insertMany(finalUser);
+      let totalUploadedUsers = uploadedUsers.length;
+      //finally we update duplicates,totalRecords,totalUploaded and discarded into database using updateOne
+      let demo = await fileModel.updateOne(
+        { _id: req.params.data },
+        {
+          $set: {
+            duplicates: duplicateUser,
+            totalRecords: totalRecords,
+            totalUploaded: totalUploadedUsers,
+            discarded: discarded,
+          },
+        }
+      );
+      if (demo) {
+        res.send({
+          type: "success",
+          message: "Data fetched",
+          data: demo,
+        });
+      } else {
+        res.json({
+          status: "error",
+          code: 404,
+          message: "File not convert CSV to JSON",
+        });
+      }
+    }
+  } catch (error) {
+    res.json({
+      status: "error",
+      code: 404,
+      message: "File not convert CSV to JSON",
+    });
+  }
+});
+
+//signup
+router.post(
+  "/signup",
+  [
+    check("email", "Email length should be 10 to 30 characters")
+      .isEmail()
+      .isLength({ min: 10, max: 30 }),
+    check("name", "Name length should be 10 to 20 characters").isLength({
+      min: 2,
+      max: 20,
+    }),
+    check("mobile", "Mobile number should contains 10 digits").isLength({
+      min: 10,
+      max: 10,
+    }),
+    check("password", "Password length should be 8 to 10 characters").isLength({
+      min: 6,
+      max: 10,
+    }),
+  ],
+  authJWT,
   async (req, res) => {
     try {
+      //code for add the data
+      const item = {
+        name: req.body.name,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        password: req.body.password,
+      };
+
       let data = await userModel.findOne({
-        //user can login mobile and email both
-        $or: [{ email: req.body.email }, { mobile: req.body.email }]
-      }).lean();
-      console.log("data", data);
+        //check if email or mobile is already exist or not
+        $or: [{ email: req.body.email }, { mobile: req.body.mobile }],
+      });
       if (data) {
-        //if we get the data then we generate the token using jsonwebtoken
-        const privateKey = "ujtqcfrdeujbjgdbhghjdbfghdjfuygfbjgfhbfghg";
-        //params is used for which field we want to add
-        let params = { userId:data._id, email: data.email, name: data.name };
-
-   
-        let token = await jwt.sign(params, privateKey, { expiresIn: '8h' });
-        if (data.password == req.body.password) {
-          //if our password is mach with the database then we send the cookies 
-          res.cookie("token", token);
-          console.log("token", token);
-          // console.log("req.headers.cookie",req.headers.cookie);
-          res.send({
-            type: "success",
-            message: 'message sucessful',
-            token: token,
-            status: 200
-          })
-
-          // res.redirect("/signup")
-
-        } else {
-          res.send("password invalid...");
-        }
+        res.send({
+          type: "error",
+          message: "this email or mobile number already used...",
+        });
       } else {
-        res.send("user not found...");
+        let dataStore = userModel(item);
+        dataStore.save(async function (err, result) {
+          if (err) {
+            res.json({
+              status: "error",
+              code: 404,
+              message: "error",
+            });
+          } else {
+            res.send({
+              type: "success",
+              result: dataStore,
+            });
+          }
+        });
       }
+
     } catch (error) {
-      console.log(error);
+      res.json({
+        status: "error",
+        code: 404,
+        message: "something went wrong",
+      });
     }
-  })
+  }
+);
+//for login api and route
+router.post("/loginApi", async (req, res) => {
+  try {
+    let data = await userModel
+      .findOne({
+        //user can login mobile and email both
+        $or: [{ email: req.body.email }, { mobile: req.body.email }],
+      })
+      .lean();
+    if (data) {
+      //if we get the data then we generate the token using jsonwebtoken
+      const privateKey = "ujtqcfrdeujbjgdbhghjdbfghdjfuygfbjgfhbfghg";
+      //params is used for which field we want to add
+      let params = { userId: data._id, email: data.email, name: data.name };
+
+      let token = await jwt.sign(params, privateKey, { expiresIn: "8h" });
+      if (data.password == req.body.password) {
+        //if our password is mach with the database then we send the cookies
+        res.cookie("token", token);
+  
+        res.send({
+          type: "success",
+          message: "message sucessful",
+          token: token,
+          status: 200,
+        });
+
+        // res.redirect("/signup")
+      } else {
+        res.send("password invalid...");
+      }
+    } else {
+      res.send("user not found...");
+    }
+  } catch (error) {
+    res.json({
+      status: "error",
+      code: 404,
+      message: "something went wrong",
+    });
+  }
+});
 
 //logout
 router.get("/logout", async (req, res) => {
@@ -329,7 +352,11 @@ router.get("/logout", async (req, res) => {
     return res.json({ status: "success", code: 200 });
   } catch (error) {
     //if error then go in error
-    console.log(error);
+    res.json({
+      status: "error",
+      code: 404,
+      message: "something went wrong",
+    });
     return res.json({
       status: "error",
       code: 404,
@@ -337,6 +364,5 @@ router.get("/logout", async (req, res) => {
     });
   }
 });
-
 
 module.exports = router;
